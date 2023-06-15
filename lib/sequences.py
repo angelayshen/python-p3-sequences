1557488d35998060ee7fe3497eae48437cfc8fb5
Angela Shen <angelayshen@gmail.com>
2023-06-15 12:41:30 -0400

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    sequence = []
+    for n in range(0, length):
+        if n < 2:
+            sequence.append(n)
+        else:
+            sequence.append(sequence[n-1] + sequence[n-2])
+    print(sequence)
